d180dccc4854d5071a1ff0bee4816ee575f75d7f
zvo <astrotrain55@gmail.com>
2019-04-15 15:51:48 +0500

    delete jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,11 @@
+var elements = document.getElementsByClassName('game__item');
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', function(e) {
+    var game = e.currentTarget.getAttribute('data-game');
+    run(game);
+  });
+}
+
 function run(game){
   alert(game);
 }
-
-$(function(){
-  $('.game__item').on('click', function(){
-    var game = $(this).data('game');
-    run(game);
-  });
-});
